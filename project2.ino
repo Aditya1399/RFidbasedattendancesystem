5b8ddfd83b3d883469644d44d26222d6ea966f9d
Aditya Dogra <43300359+Aditya1399@users.noreply.github.com>
2020-01-24 19:23:34 +0530

    Add files via upload

---
+++ project2.ino
@@ -0,0 +1,100 @@
+
+#include "ESP8266WiFi.h"
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+// WiFi parameters
+#define WLAN_SSID       "Galaxy M30sFF81"
+#define WLAN_PASS       "prakhar99"
+
+// Adafruit IO
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883
+#define AIO_USERNAME    "cooladi77"
+#define AIO_KEY         "aio_veyf20aX9cw2dJkk0r1FtaCzpqKU"  // Obtained from account info on io.adafruit.com
+
+// Create an ESP8266 WiFiClient class to connect to the MQTT server.
+WiFiClient client;
+
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+
+Adafruit_MQTT_Publish Attendance = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/RFID_Attendance");
+
+char ID;
+/*************************** Sketch Code ************************************/
+
+void connect() {
+  Serial.print(F("Connecting to Adafruit IO... "));
+  int8_t ret;
+  while ((ret = mqtt.connect()) != 0) {
+    switch (ret) {
+      case 1: Serial.println(F("Wrong protocol")); break;
+      case 2: Serial.println(F("ID rejected")); break;
+      case 3: Serial.println(F("Server unavail")); break;
+      case 4: Serial.println(F("Bad user/pass")); break;
+      case 5: Serial.println(F("Not authed")); break;
+      case 6: Serial.println(F("Failed to subscribe")); break;
+      default: Serial.println(F("Connection failed")); break;
+    }
+
+    if(ret >= 0)
+      mqtt.disconnect();
+
+    Serial.println(F("Retrying connection..."));
+    delay(5000);
+  }
+  Serial.println(F("Adafruit IO Connected!"));
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println(F("Adafruit IO Example"));
+
+  // Connect to WiFi access point.
+  Serial.println(); Serial.println();
+  delay(10);
+  Serial.print(F("Connecting to "));
+  Serial.println(WLAN_SSID);
+
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(F("."));
+  }
+  Serial.println();
+
+  Serial.println(F("WiFi connected"));
+  Serial.println(F("IP address: "));
+  Serial.println(WiFi.localIP());
+
+  // connect to adafruit io
+  connect();
+
+}
+
+// connect to adafruit io via MQTT
+
+
+void loop() {
+  // ping adafruit io a few times to make sure we remain connected
+  if(! mqtt.ping(3)) {
+    // reconnect to adafruit io
+    if(! mqtt.connected())
+      connect();
+  }
+   if ( Serial.available() ) { // Update and send only after 1 seconds
+   char a = Serial.read();
+   ID = a;  
+   
+   if (! Attendance.publish(ID)) {                     //Publish to Adafruit
+      Serial.println(F("Failed"));
+    } else {
+      Serial.println(F("Sent!"));
+    }
+}
+}
+//view rawNodemcu Code for RFID Attendance.ino hosted with ❤ by GitHub
+//Code Explanation
+//1. Arduino Code:
+
+
